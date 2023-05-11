bbe46ec32bce26154652f02977a814a708c2ee2b
Yue Yu <yue.yu@superfish.me>
2023-05-11 01:12:31 -0400

    wip: export metadata

--- src/GhidraMetaDump.java
+++ src/GhidraMetaDump.java
@@ -16,15 +16,60 @@ public class GhidraMetaDump extends GhidraScript {
         ifc.setOptions(options);
         ifc.openProgram(program);
 
-        for (var func : program.getFunctionManager().getFunctions(/* forward: */ true)) {
-            var res = ifc.decompileFunction(func, 30, null);
-            var hF = res.getHighFunction();
+        var tbl = program.getSymbolTable();
 
+        for (var func : program.getFunctionManager().getFunctions(/* forward: */ false)) {
+            var res = ifc.decompileFunction(func, 30, null);
+
+            if (!res.decompileCompleted()) {
+                printf("Decompile failed for %s\n", func.getName());
+                continue;
+            }
+
+            // if (!func.getName().equals("main")) {
+            // continue;
+            // }
+
+            var hF = res.getHighFunction();
             if (hF == null) {
                 continue;
             }
 
-            printf("%s\n", hF.getFunction().getName());
+            printf("Name: %s\n", hF.getFunction().getName());
+            printf("  entry: 0x%s\n", func.getBody().getMinAddress());
+            printf("  exit: 0x%s\n", func.getBody().getMaxAddress());
+
+            print("  local symbols:\n");
+            res.getHighFunction().getLocalSymbolMap().getSymbols().forEachRemaining((symbol) -> {
+                printf("    name:  %s\n", symbol.getName());
+                printf("    type:  %s\n", symbol.getDataType());
+                printf("    addr:  0x%s\n", symbol.getPCAddress());
+                if (symbol.getHighVariable() != null) {
+                    var hV = symbol.getHighVariable();
+                    printf("    reps:  %s\n", hV.getRepresentative());
+                    print("    varnodes:\n");
+                    for (var vnode : hV.getInstances()) {
+                        printf("      addr: %s, pcode: %s\n", vnode.getPCAddress(), vnode);
+                    }
+                }
+            });
+
+            print("  global symbols:\n");
+            res.getHighFunction().getGlobalSymbolMap().getSymbols().forEachRemaining((symbol) -> {
+                printf("    name:  %s\n", symbol.getName());
+                printf("    type:  %s\n", symbol.getDataType());
+                printf("    addr:  0x%s\n", symbol.getPCAddress());
+                if (symbol.getHighVariable() != null) {
+                    var hV = symbol.getHighVariable();
+                    printf("    reps:  %s\n", hV.getRepresentative());
+                    print("    varnodes:\n");
+                    for (var vnode : hV.getInstances()) {
+                        printf("      addr: %s, pcode: %s\n", vnode.getPCAddress(), vnode);
+                    }
+                }
+            });
+
+
         }
     }
 }
