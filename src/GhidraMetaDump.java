7878eb192a75eb57f1adf2f7b871b6fabbb6f928
Yue Yu <yue.yu@superfish.me>
2023-05-24 17:41:14 +0800

    update: Dump meta data fron ghidra

--- src/GhidraMetaDump.java
+++ src/GhidraMetaDump.java
@@ -1,75 +1,32 @@
 // Dumps the pcode into a nested json.
 // @category PCode
 
-import ghidra.app.decompiler.DecompInterface;
-import ghidra.app.decompiler.DecompileOptions;
+import java.io.FileWriter;
+
+import com.google.gson.GsonBuilder;
+
+import entity.Metadata;
 import ghidra.app.script.GhidraScript;
 
 public class GhidraMetaDump extends GhidraScript {
 
     @Override
     public void run() throws Exception {
-        var program = currentProgram;
+        var meta = new Metadata(currentProgram);
 
-        var ifc = new DecompInterface();
-        var options = new DecompileOptions();
-        ifc.setOptions(options);
-        ifc.openProgram(program);
+        var builder = new GsonBuilder();
+        var gson = builder.create();
+        var json = gson.toJson(meta);
 
-        var tbl = program.getSymbolTable();
-
-        for (var func : program.getFunctionManager().getFunctions(/* forward: */ false)) {
-            var res = ifc.decompileFunction(func, 30, null);
-
-            if (!res.decompileCompleted()) {
-                printf("Decompile failed for %s\n", func.getName());
-                continue;
-            }
-
-            // if (!func.getName().equals("main")) {
-            // continue;
-            // }
-
-            var hF = res.getHighFunction();
-            if (hF == null) {
-                continue;
-            }
-
-            printf("Name: %s\n", hF.getFunction().getName());
-            printf("  entry: 0x%s\n", func.getBody().getMinAddress());
-            printf("  exit: 0x%s\n", func.getBody().getMaxAddress());
-
-            print("  local symbols:\n");
-            res.getHighFunction().getLocalSymbolMap().getSymbols().forEachRemaining((symbol) -> {
-                printf("    name:  %s\n", symbol.getName());
-                printf("    type:  %s\n", symbol.getDataType());
-                printf("    addr:  0x%s\n", symbol.getPCAddress());
-                if (symbol.getHighVariable() != null) {
-                    var hV = symbol.getHighVariable();
-                    printf("    reps:  %s\n", hV.getRepresentative());
-                    print("    varnodes:\n");
-                    for (var vnode : hV.getInstances()) {
-                        printf("      addr: %s, pcode: %s\n", vnode.getPCAddress(), vnode);
-                    }
-                }
-            });
-
-            print("  global symbols:\n");
-            res.getHighFunction().getGlobalSymbolMap().getSymbols().forEachRemaining((symbol) -> {
-                printf("    name:  %s\n", symbol.getName());
-                printf("    type:  %s\n", symbol.getDataType());
-                printf("    addr:  0x%s\n", symbol.getPCAddress());
-                if (symbol.getHighVariable() != null) {
-                    var hV = symbol.getHighVariable();
-                    printf("    reps:  %s\n", hV.getRepresentative());
-                    print("    varnodes:\n");
-                    for (var vnode : hV.getInstances()) {
-                        printf("      addr: %s, pcode: %s\n", vnode.getPCAddress(), vnode);
-                    }
-                }
-            });
-
-
+        var file = askFile("Save file", "OK");
+        if (file.createNewFile()) {
+            println("New file is created");
+        } else {
+            println("File already exists, will be overwritten");
         }
+
+        var writer = new FileWriter(file, false);
+        writer.write(json);
+        writer.close();
     }
 }
---
+++ src/entity/ControlFlowGraph.java
@@ -0,0 +1,135 @@
+package entity;
+
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import ghidra.program.model.pcode.HighFunction;
+import ghidra.program.model.pcode.PcodeBlockBasic;
+import ghidra.program.model.pcode.PcodeOp;
+import utils.Utils;
+
+public class ControlFlowGraph {
+    public class BasicBlockContext {
+        public List<String> preds = new ArrayList<String>();
+        public List<String> succs = new ArrayList<String>();
+
+        public BasicBlockContext(PcodeBlockBasic pcodeBlockBasic) {
+            for (int i = 0; i < pcodeBlockBasic.getInSize(); i++) {
+                var in = pcodeBlockBasic.getIn(i);
+                preds.add(in.getStart().toString());
+            }
+
+            for (int i = 0; i < pcodeBlockBasic.getOutSize(); i++) {
+                var out = pcodeBlockBasic.getOut(i);
+                succs.add(out.getStart().toString());
+            }
+        }
+
+        public List<String> getPreds() {
+            return preds;
+        }
+
+        public void setPreds(List<String> preds) {
+            this.preds = preds;
+        }
+
+        public List<String> getSuccs() {
+            return succs;
+        }
+
+        public void setSuccs(List<String> succs) {
+            this.succs = succs;
+        }
+    }
+
+    public class PCodeContext {
+        public List<String> preds = new ArrayList<String>();
+        public List<String> succs = new ArrayList<String>();
+
+        public PCodeContext(Function ctx, PcodeOp pcode) {
+            // two cases:
+            // 1. if a pred and current are in the same BB, just insert and exit
+            // 2. if not, visit the last/first of pred/succ bb
+
+            var current = pcode.getParent();
+            var inBlockPcodes = ctx.basicblocks.get(current.getStart().toString()).pcodes;
+
+            if (0 <= pcode.getSeqnum().getOrder() - 1) {
+                // pred is in the same basic block
+                preds.add(Utils.PCodeId(pcode.getParent().getStart().toString(), pcode.getSeqnum().getOrder() - 1));
+            } else {
+                // find the pred bb and add the last
+                for (int i = 0; i < current.getInSize(); i++) {
+                    var pred = current.getIn(i);
+                    var predBlockPcodes = ctx.basicblocks.get(pred.getStart().toString()).pcodes;
+                    var predLast = predBlockPcodes.get(predBlockPcodes.size() - 1);
+                    preds.add(predLast);
+                }
+            }
+
+            if ((pcode.getSeqnum().getOrder() + 1) < inBlockPcodes.size()) {
+                // pred is in the same basic block
+                succs.add(Utils.PCodeId(pcode.getParent().getStart().toString(), pcode.getSeqnum().getOrder() + 1));
+            } else {
+                // find the succ bb and add the first
+                for (int i = 0; i < current.getOutSize(); i++) {
+                    var succ = current.getOut(i);
+                    var succBlockPcodes = ctx.basicblocks.get(succ.getStart().toString()).pcodes;
+                    var succLast = succBlockPcodes.get(0);
+                    succs.add(succLast);
+                }
+            }
+        }
+
+        public List<String> getPreds() {
+            return preds;
+        }
+
+        public void setPreds(List<String> preds) {
+            this.preds = preds;
+        }
+
+        public List<String> getSuccs() {
+            return succs;
+        }
+
+        public void setSuccs(List<String> succs) {
+            this.succs = succs;
+        }
+    }
+
+    public Map<String, BasicBlockContext> basicblocks = new HashMap<>();
+
+    public Map<String, PCodeContext> pcodes = new HashMap<>();
+
+    public Map<String, BasicBlockContext> getBasicblocks() {
+        return basicblocks;
+    }
+
+    public void setBasicblocks(Map<String, BasicBlockContext> basicblocks) {
+        this.basicblocks = basicblocks;
+    }
+
+    public Map<String, PCodeContext> getPcodes() {
+        return pcodes;
+    }
+
+    public void setPcodes(Map<String, PCodeContext> pcodes) {
+        this.pcodes = pcodes;
+    }
+
+    public ControlFlowGraph(Function ctx, HighFunction hF) {
+        for (var basicblock : hF.getBasicBlocks()) {
+            basicblocks.put(basicblock.getStart().toString(), new BasicBlockContext(basicblock));
+
+            var it = basicblock.getIterator();
+            while (it.hasNext()) {
+                var pcodeOp = it.next();
+
+                pcodes.put(Utils.PCodeId(pcodeOp), new PCodeContext(ctx, pcodeOp));
+            }
+        }
+    }
+}
